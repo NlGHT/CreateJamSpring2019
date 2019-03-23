d98474f218ebebb1ad07977079e88cb63df7266a
PolleAnker <thementoryay@gmail.com>
2019-03-23 14:03:02 +0100

    two players in scene, early finish line

---
+++ Assets/FinishLine.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    // When a collider enters the collider
+    void OnTriggerEnter(Collider other)
+    {
+        MeshRenderer meshRend = GetComponent<MeshRenderer>();
+        meshRend.material.color = Color.green;
+    }
+}
