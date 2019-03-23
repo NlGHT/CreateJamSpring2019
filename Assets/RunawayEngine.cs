35a4dd04e6a1b8ad72c51a257ac2278cda7f846a
Stefan Nordborg Eriksen <the-grey-side@hotmail.com>
2019-03-23 14:42:40 +0100

    Adding runaway engine code

---
+++ Assets/RunawayEngine.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunawayEngine : MonoBehaviour
+{
+    [SerializeField]
+    private bool detached;
+    private Rigidbody rigid;
+    public float thrustForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (detached)
+        {
+            ThrustForward();
+        }
+    }
+
+    public void Detach()
+    {
+
+    }
+
+    private void ThrustForward()
+    {
+        rigid.AddForce(transform.right * thrustForce);
+    }
+}
